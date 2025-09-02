934360cc6635e4c99bebd19ae4d0846770a39412
HamzaNabeel <hamzanabeel085@gmail.com>
2025-09-03 01:24:07 +0530

    adding my first practice project Weather app

---
+++ Weather app/script.js
@@ -0,0 +1,40 @@
+const apiKey = "23d9e5e95eed0f2a81400d3cc6975d61";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°C`;    
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";    
+    document.querySelector(".wind").innerHTML = data.wind.speed + "     km/h";    
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png"
+    }else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/drizzle.png"
+    }else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png"
+    }
+
+    document.querySelector(".weather").style.display = "block";
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
+
+searchBox.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
+});
+
+// checkWeather()
